0ec45ecef432d58f5a80f3b3bd655c8b1d2d3dd7
Mathias Verbeke <mathias.verbeke@student.howest.be>
2023-02-11 00:25:45 +0100

    Adding additional feature: unique lines identifier (-u option)

--- ExcelComparisonTool.py
+++ ExcelComparisonTool.py
@@ -5,7 +5,9 @@
 # Date of creation: 2022/02/10
 # Summary: This app takes two excel files as input, converts them into two csv files 
 # and performs a comparison to check for any common lines between them. If any common 
-# data lines are found, they are written to a new output excel file.
+# data lines are found, they are written to a new output excel file. If the unique
+# option is provided, the lines in file1 that are not present in file2 are written to 
+# the new output excel file.
 #####################################################################################
 
 import argparse, csv
@@ -20,6 +22,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('filename1')
 parser.add_argument('filename2')
+parser.add_argument('-u', '--unique', action = 'store_true', help = 'use this option to see what lines in file1 are not present in file2')
 
 args = parser.parse_args()
 
@@ -43,21 +46,27 @@ read_file2.to_csv(csv_filename2, index = None, header=True)
 # Comparing csv contents
 ########################
 
-########################
-# Comparing csv contents
-########################
+# Looking for common lines
 
 with open(csv_filename1, 'r') as csv_file1:
     csv_reader1 = csv.reader(csv_file1, delimiter = ',')
-    wb = Workbook()
-    ws = wb.active
-    ws.title = "Common Lines"
     flag = "header"
-    counter = 0
+
+    if args.unique != True:
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Common Lines"
+
+    else:
+        CommonLines = []
+    
+    CounterCommon = 0
 
     for line1 in csv_reader1:
         if flag == "header":
-            ws.append(line1)
+            if args.unique != True:
+                ws.append(line1)
+            
             flag = "data"
             continue
 
@@ -66,9 +75,48 @@ with open(csv_filename1, 'r') as csv_file1:
 
             for line2 in csv_reader2:
                 if line1 == line2:
-                    ws.append(line1)
-                    counter += 1
 
-wb.save("CommonLines.xlsx")
+                    if args.unique != True:
+                        ws.append(line1)
+                        CounterCommon += 1
 
-print("{} identical data lines.".format(counter))
+                    else:
+                        CommonLines.append(line1)
+                        
+# Looking for distinct lines in file1
+
+print(CommonLines)
+
+if args.unique == True and len(CommonLines) == 0:
+    print("There are no unique lines in {}".format(args.filename1))
+
+elif args.unique == True:
+    with open(csv_filename1, 'r') as csv_file1:
+        csv_reader1 = csv.reader(csv_file1, delimiter = ',')
+        wb = Workbook()
+        ws = wb.active
+        flag = "header"
+        ws.title = "Unique Lines"
+        
+        CounterUnique = 0
+
+        for line in csv_reader1:
+            if flag == "header":
+                ws.append(line)
+                flag = "data"
+                continue
+
+            if line in CommonLines:
+                continue
+
+            else:
+                ws.append(line)
+                CounterUnique += 1
+
+if args.unique != True:
+    wb.save("CommonLines.xlsx")
+    print("{} identical data lines.".format(CounterCommon))
+
+else:
+    wb.save("UniqueLines.xlsx")
+    print("{} unique data lines.".format(CounterUnique))
